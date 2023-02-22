fbb593677844188ada64ad0b8c227632c02ffbb7
MeldrickQuiroz <111593710+MeldrickQuiroz@users.noreply.github.com>
2023-02-22 14:13:17 -0800

    Update FileMover.py

--- FileMover.py
+++ FileMover.py
@@ -2,7 +2,7 @@ import shutil
 
 destination = '/This/is/where/the/file/will/go'
 
-filepaths = []
+filepaths = ["This is the file you want to move using filepaths"]
 
 for i in range(0, len(filepaths)):
     shutil.move(filepaths[i], destination)
